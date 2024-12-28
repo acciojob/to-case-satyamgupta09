c16b7d8bef55f1fe638fdd94036e9fa7fd699813
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2024-12-28 14:23:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function toCase(text) {
   // write your code here
+	if(text.length==0) return '-';
+	let s1=text.toLowerCase().trim();
+	let s2=text.toUpperCase().trim();
+
+	return (s1+'-'+s2);
 }
 
 // DO not change the code below
 
-const text = prompt("Enter text:");
+// const text = prompt("Enter text:");
 alert(toCase(text));
